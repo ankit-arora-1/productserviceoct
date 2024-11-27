0748932008cf8f5e27e094a30d9dc05f89f58d38
Ankit Arora <ankit@192.168.1.12>
2024-11-27 16:45:21 +0530

    Added flyway

--- src/main/java/com/scaler/productservice/models/Category.java
+++ src/main/java/com/scaler/productservice/models/Category.java
@@ -3,9 +3,12 @@ package com.scaler.productservice.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -15,7 +18,9 @@ import java.util.List;
 public class Category extends BaseModel {
     private String name;
 
-    @OneToMany(mappedBy = "category", cascade = {CascadeType.REMOVE})
+    @OneToMany(mappedBy = "category", cascade = {CascadeType.REMOVE},
+    fetch = FetchType.EAGER)
     @JsonIgnore
+    @Fetch(FetchMode.JOIN)
     private List<Product> products;
 }
--- src/test/java/com/scaler/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/scaler/productservice/ProductServiceApplicationTests.java
@@ -1,11 +1,14 @@
 package com.scaler.productservice;
 
+import com.scaler.productservice.models.Category;
 import com.scaler.productservice.models.Product;
+import com.scaler.productservice.repositories.CategoryRepository;
 import com.scaler.productservice.repositories.ProductRepository;
 import com.scaler.productservice.repositories.projections.ProductTitleAndDesc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -16,6 +19,9 @@ class ProductServiceApplicationTests {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
 
     @Test
@@ -56,6 +62,25 @@ class ProductServiceApplicationTests {
 
 
         System.out.println(product.getTitle() + ", " + product.getDescription());
+    }
+
+    @Test
+    @Transactional
+    public void testFetchTypes() {
+        Optional<Category> category = categoryRepository.findById(1L);
+        System.out.println(category.get().getName());
+        System.out.println(category.get().getProducts());
+    }
+
+    @Test
+    @Transactional
+    public void nplus1Problem() {
+        List<Category> categories = categoryRepository.findAll();
+        for(Category category: categories) {
+            for(Product product: category.getProducts()) {
+                System.out.println(product.getTitle());
+            }
+        }
     }
 
 }
