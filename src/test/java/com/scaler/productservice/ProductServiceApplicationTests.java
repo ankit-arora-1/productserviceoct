ead1c19b01d4bb410a6955dd00da38e241cc89b8
Ankit Arora <ankit@192.168.1.12>
2024-11-30 09:35:59 +0530

    Added env variables

--- src/test/java/com/scaler/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/scaler/productservice/ProductServiceApplicationTests.java
@@ -28,59 +28,59 @@ class ProductServiceApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    public void test() {
-        Optional<Product> productOptional = productRepository
-                .findByTitleAndCategory_name("iPhone", "phones");
-
-
-        System.out.println(productOptional.get().getTitle());
-    }
-
-    @Test
-    public void test2() {
-        List<Product> productOptional = productRepository
-                .getProductDate("phones");
-
-
-        System.out.println(productOptional.get(0).getTitle());
-    }
-
-    @Test
-    public void test3() {
-        Product productOptional = productRepository
-                .getProductData2(1L);
-
-
-        System.out.println(productOptional.getTitle());
-    }
-
-    @Test
-    public void test4() {
-        ProductTitleAndDesc product = productRepository
-                .getProductData3(1L);
-
-
-        System.out.println(product.getTitle() + ", " + product.getDescription());
-    }
-
-    @Test
-    @Transactional
-    public void testFetchTypes() {
-        Optional<Category> category = categoryRepository.findById(1L);
-        System.out.println(category.get().getName());
-        System.out.println(category.get().getProducts());
-    }
-
-    @Test
-    @Transactional
-    public void nplus1Problem() {
-        List<Category> categories = categoryRepository.findAll();
-        for(Category category: categories) {
-            for(Product product: category.getProducts()) {
-                System.out.println(product.getTitle());
-            }
-        }
-    }
+//    @Test
+//    public void test() {
+//        Optional<Product> productOptional = productRepository
+//                .findByTitleAndCategory_name("iPhone", "phones");
+//
+//
+//        System.out.println(productOptional.get().getTitle());
+//    }
+//
+//    @Test
+//    public void test2() {
+//        List<Product> productOptional = productRepository
+//                .getProductDate("phones");
+//
+//
+//        System.out.println(productOptional.get(0).getTitle());
+//    }
+//
+//    @Test
+//    public void test3() {
+//        Product productOptional = productRepository
+//                .getProductData2(1L);
+//
+//
+//        System.out.println(productOptional.getTitle());
+//    }
+//
+//    @Test
+//    public void test4() {
+//        ProductTitleAndDesc product = productRepository
+//                .getProductData3(1L);
+//
+//
+//        System.out.println(product.getTitle() + ", " + product.getDescription());
+//    }
+//
+//    @Test
+//    @Transactional
+//    public void testFetchTypes() {
+//        Optional<Category> category = categoryRepository.findById(1L);
+//        System.out.println(category.get().getName());
+//        System.out.println(category.get().getProducts());
+//    }
+//
+//    @Test
+//    @Transactional
+//    public void nplus1Problem() {
+//        List<Category> categories = categoryRepository.findAll();
+//        for(Category category: categories) {
+//            for(Product product: category.getProducts()) {
+//                System.out.println(product.getTitle());
+//            }
+//        }
+//    }
 
 }
